51e7e7e33b7a4c5348e8ad9cab4217dc939a0a0b
Henrik Lindelöf Bilski <hlb@netcompany.com>
2020-02-26 10:36:16 +0100

    Added working dummy code for 2.5

--- src/main/groovy/ItemDataMapper.groovy
+++ src/main/groovy/ItemDataMapper.groovy
@@ -18,6 +18,10 @@ class ItemDataMapper extends Mapping {
         b.type = { ->
             return a.type ? Itemtype.valueOf(a.type) : INVALID_TYPE
         }
+        // The following code works in Groovy 2.5
+        //b.type = { ->
+        //    return a.data.type ? Itemtype.valueOf(a.data.type) : INVALID_TYPE
+        //}
     }
 
 }
